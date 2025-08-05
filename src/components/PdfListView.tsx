21e15475701b30f2d47a75f728bad751c6e7ad91
宋花生 <huashenyay@gmail.com>
2025-08-05 06:39:24 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/components/PdfListView.tsx
+++ src/components/PdfListView.tsx
@@ -20,7 +20,7 @@ import {
   AlertDialogTrigger,
 } from "@/components/ui/alert-dialog"
 import { cn } from '@/lib/utils';
-import { Tooltip, TooltipProvider, TooltipTrigger } from './ui/tooltip';
+import { Tooltip, TooltipProvider, TooltipTrigger, TooltipContent } from './ui/tooltip';
 
 
 interface PdfFile {
